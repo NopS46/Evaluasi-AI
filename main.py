b902521f04fff5c4231cb54a8a97b1b06fbd58f1
NopS46 <cumanthaka4@gmail.com>
2025-08-03 10:16:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def get_db():
 # CORS settings
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Boleh disesuaikan saat produksi
+    allow_origins=["https://nops46.github.io"],  # Boleh disesuaikan saat produksi
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
